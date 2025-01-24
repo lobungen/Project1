4e8620da47d64cb17cc064c37d2f1b7885f3290b
tiffany clark <tiffanymarie9811@gmail.com>
2025-01-23 22:36:44 -0500

    updateJS

--- assets/Login.js
+++ assets/Login.js
@@ -1,3 +1,61 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const loginButton = document.getElementById('login');
+
+    if (loginButton) {
+        loginButton.addEventListener('click', function (event) {
+            event.preventDefault();
+
+
+            const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    if (username === '' || password === '') {
+        alert("Cannot be empty");
+        return;
+    }
+        
+    localStorage.setItem('username', username);
+    localStorage.setItem('password', password);
+
+
+    window.location.href = 'main\main.html'; 
+});
+    }
+});
+
+
+function renderLastRegisteredUser() {
+    const storedUsername = localStorage.getItem('username');
+    const storedPassword = localStorage.getItem('password');
+    
+    if (storedUsername && storedPassword) {
+    
+        console.log(`Stored Username: ${storedUsername}`);
+    
+        console.log(`Stored Password: ${storedPassword}`);
+    
+    } else {
+        console.log("No user data found in localStorage.");
+    };
+}
+
+    
+    function redirectPage(url) {
+        window.location.href = url; 
+}
+
+
+    renderLastRegisteredUser();
+
+
+
+
+
+
+
+
+/*
 const email = document.querySelector('email');
 const password = document.querySelector('password');
 const login = document.querySelector('login');
@@ -15,3 +73,4 @@ login.addEventListener('click', () => {
 localStorage.setItem('email', email);
 localStorage.setItem('password', password);
 renderLastRegisteredUser();
+*/
